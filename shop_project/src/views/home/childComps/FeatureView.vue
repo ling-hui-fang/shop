4ddec58b45bcbc7bcbab0de2114b2e1092eb1a26
ling-hui-fang <yanglingyang@aliyun.com>
2019-12-20 16:24:03 +0800

    首页home完成

---
+++ shop_project/src/views/home/childComps/FeatureView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="feature">
+        <a href="http://act.mogujie.com/zzlx67">
+            <img src="~assets/img/home/recommend_bg.jpg" alt="">
+        </a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FeatureView',
+
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.feature img {
+    width:100%;
+}
+</style>
