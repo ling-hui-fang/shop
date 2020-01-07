44eb58c58db11e28c142446986beaa3d1a7360b6
linghuifang <834979382@qq.com>
2020-01-07 17:55:13 +0800

    登录功能的完成

--- vue-manage/src/main.js
+++ vue-manage/src/main.js
@@ -6,6 +6,10 @@ import './plugins/element.js'
 import 'assets/css/global.css'
 import 'assets/font/iconfont.css'
 
+import axios from 'axios'
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
--- vue-manage/src/router/index.js
+++ vue-manage/src/router/index.js
@@ -4,6 +4,8 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const Login = () => import('components/Login')
+const Home = () => import('components/Home')
+
 const routes = [
   {
     path:"/",
@@ -12,12 +14,30 @@ const routes = [
   {
     path:"/login",
     component:Login
+  },
+  {
+    path:'/home',
+    component:Home
   }
 ]
 
 const router = new VueRouter({
     routes,
     mode:"history"
+})
+
+//挂载路由导航守卫
+router.beforeEach((to,from,next)=>{
+  //to 将访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  //next() 放行 next('/login)强制跳转
+
+  if(to.path === "/login") return next()
+  //获取token
+  const tokenStr = window.sessionStorage.getItem("token")
+  if(!tokenStr) return next('/login')
+  next()
 })
 
 export default router
