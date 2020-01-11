f3dca6c022fb8fd01fa97c5c596e627f68cb81a5
linghuifang <834979382@qq.com>
2020-01-11 09:33:36 +0800

    提交登录

--- vue-manage/src/components/Home.vue
+++ vue-manage/src/components/Home.vue
@@ -1,19 +1,95 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
+    <el-container class="home-container">
+        <el-header>
+            <div>
+                <img src="~assets/img/home_logo.png" alt="">
+                <span>电商后台管理系统</span>
+            </div>
+            <el-button type="info" @click="logout">退出</el-button>
+        </el-header>
+        <el-container>
+            <el-aside width="200px">
+                <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+                    <!-- 一级菜单 -->
+                    <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
+                        <!-- 一级菜单的模板区 -->
+                        <template slot="title">
+                            <!-- 图标 -->
+                            <i class="el-icon-location"></i>
+                            <!-- 文本 -->
+                            <span>{{item.authName}}</span>
+                        </template>
+
+                        <!-- 二级菜单 -->
+                        <el-menu-item :index="subItem.id + ''" v-for="subItem in item.children">
+                            <template slot="title">
+                                <!-- 图标 -->
+                                <i class="el-icon-location"></i>
+                                <!-- 文本 -->
+                                <span>{{subItem.authName}}</span>
+                            </template>
+                        </el-menu-item>
+                        <!-- <el-submenu index="1-4">
+                            <template slot="title">选项4</template>
+                            <el-menu-item index="1-4-1">选项1</el-menu-item>
+                        </el-submenu> -->
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
 </template>
 <script>
 export default {
     name:"Home",
+    data(){
+        return{
+            menulist:[]
+        }
+    },
+    created(){
+        this.getMenuList()
+    },
     methods:{
         logout(){
             window.sessionStorage.clear()
             this.$router.push('/login')
+        },
+        async getMenuList(){
+            const { data: res } = await this.$http.get('menus')
+            console.log(res);
+            if(res.meta.status !== 200) return this.$message.error(res.meta.msg)
+            this.menulist = res.data
         }
     }
 }
 </script>
-<style lang="">
-    
+<style lang="less" scoped>
+    .el-header{
+        background-color: #373d41;
+        color:white;
+        display: flex;
+        justify-content:space-between;
+        padding-left:0;
+        align-items:center;
+        > div {
+            display:flex;
+            align-items:center;
+        }
+        span {
+            margin-left:15px;
+            font-size:20px;
+        }
+    }
+    .el-aside{
+        background-color: #333744;
+        color:white;
+    }
+    .el-main{
+        background-color: #eaedf1;
+    }
+    .home-container{
+        height:100%;
+    }
 </style>
--- vue-manage/src/main.js
+++ vue-manage/src/main.js
@@ -8,6 +8,11 @@ import 'assets/font/iconfont.css'
 
 import axios from 'axios'
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+axios.interceptors.request.use(config =>{
+  // console.log(config);
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
