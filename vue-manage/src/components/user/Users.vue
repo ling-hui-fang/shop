a7f93bdd2f097a2465a443bd2beb6b6099a8e0d4
linghuifang <834979382@qq.com>
2020-01-17 17:28:34 +0800

    添加用户

--- vue-manage/src/components/user/Users.vue
+++ vue-manage/src/components/user/Users.vue
@@ -54,13 +54,28 @@
         </el-card>
 
         <!-- 添加用户的对话框 -->
-        <el-dialog title="提示" :visible.sync="addDialogVisible" width="50%">
+        <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%" @close="addDialogClosed">
             <!-- 内容主体区域 -->
-            <span>这是一段信息</span>
+            <span>
+                <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px">
+                    <el-form-item label="用户名" prop="username">
+                        <el-input v-model="addForm.username"></el-input>
+                    </el-form-item>
+                    <el-form-item label="密码" prop="password">
+                        <el-input v-model="addForm.password"></el-input>
+                    </el-form-item>
+                    <el-form-item label="邮箱" prop="email">
+                        <el-input v-model="addForm.email"></el-input>
+                    </el-form-item>
+                    <el-form-item label="手机" prop="mobile">
+                        <el-input v-model="addForm.mobile"></el-input>
+                    </el-form-item>
+                </el-form>
+            </span>
             <!-- 底部区域 -->
             <span slot="footer" class="dialog-footer">
                 <el-button @click="addDialogVisible = false">取 消</el-button>
-                <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
+                <el-button type="primary" @click="addUser" >确 定</el-button>
             </span>
         </el-dialog>
     </div>
@@ -69,6 +84,26 @@
 export default {
     name:"Users",
     data(){
+        //验证邮箱的规则
+            var checkEmail = (rule,value,cb) => {
+                //验证邮箱的正则表达式
+                const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
+
+                if(regEmail.test(value)){
+                    return cb()
+                } 
+                cb(new Error('请输入合法的邮箱'))
+            }
+            
+            //验证手机号的规则
+            var checkMobile = (rule,value,cb) => {
+                const regMobile = /^(0|86|17951)?(13[0-9]|15[0123456789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
+
+                if(regMobile.test(value)){
+                    return cb()
+                }
+                cb(new Error('请输入合法手机号'))
+            }
         return{
             //获取用户列表的参数对象
             queryInfo:{
@@ -78,7 +113,33 @@ export default {
             },
             userlist:[],
             total:0,
-            addDialogVisible:false
+            addDialogVisible:false,
+            //添加用户的表单数据
+            addForm:{
+                username:"",
+                password:"",
+                email:"",
+                mobile:""
+            },
+            //添加表单的验证规则对象
+            addFormRules:{
+                username:[
+                    { required: true, message: '请输入用户名', trigger: 'blur' },
+                    { min: 3, max: 10, message: '用户名的长度在 3 到 10 个字符之间', trigger: 'blur' }
+                ],
+                password:[
+                     { required: true, message: '请输入密码', trigger: 'blur' },
+                     { min: 6, max: 15, message: '用户名的长度在 6 到 15 个字符之间', trigger: 'blur' }
+                ],
+                email:[
+                    { required: true, message: '请输入邮箱', trigger: 'blur' },
+                    { validator:checkEmail,trigger:'blur'}
+                ],
+                mobile:[
+                    { required: true, message: '请输入手机号', trigger: 'blur' },
+                    { validator: checkMobile, trigger: 'blur' }
+                ]
+            }
         }
     },
     created() {
@@ -117,6 +178,32 @@ export default {
                userinfo.mg_state = !userinfo.mg_state
            }
            this.$message.success("更新用户状态成功")
+        },
+        //监听添加用户对话框的关闭事件
+        addDialogClosed(){
+            this.$refs.addFormRef.resetFields()
+        },
+        //点击按钮，添加新用户
+        addUser(){
+            this.$refs.addFormRef.validate(async valid => {
+                console.log(valid);
+                //如果校验失败
+                if(!valid) return 
+                //可以发起添加用户的网络请求
+               const { data:res } = await this.$http.post('users',this.addForm)
+               console.log(res);
+               if(res.meta.status !== 201) {
+                   this.$message.error('添加用户失败！')
+               }
+                this.$message.success('添加用户成功！')
+
+                //隐藏添加用户的对话框
+                this.addDialogVisible = false
+
+                //重新获取用户列表数据
+                this.getUserList()
+
+            })
         }
     }
     
